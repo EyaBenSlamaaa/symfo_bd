0224a8e5ac410ed018f51344c048ddec085bf932
EyaBenSlamaaa <eya.benslama.eya@gmail.com>
2025-04-20 21:10:27 +0100

    first commit

---
+++ tests/Controller/PagesControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class PagesControllerTest extends WebTestCase{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/pages');
+
+        self::assertResponseIsSuccessful();
+    }
+}
